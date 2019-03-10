3dfd4cd067b5aee2edb4806a84af02c3b8cefa6e
Angela Castagno <oesed16@gmail.com>
2019-03-09 23:45:24 -0500

    Updates

---
+++ precache-manifest.456da3c7785b9d9948a79eb8313a7147.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3def3af82a033c3a2cf",
+    "url": "/react-posts-mas-votados/static/js/runtime~main.2689bad8.js"
+  },
+  {
+    "revision": "a0ead5abc8ed5e25e1d2",
+    "url": "/react-posts-mas-votados/static/js/main.58455e0e.chunk.js"
+  },
+  {
+    "revision": "8fec49f024647f6da01e",
+    "url": "/react-posts-mas-votados/static/js/2.c0e12582.chunk.js"
+  },
+  {
+    "revision": "a0ead5abc8ed5e25e1d2",
+    "url": "/react-posts-mas-votados/static/css/main.59db1f68.chunk.css"
+  },
+  {
+    "revision": "46575411e4df1c87edabae8fd3092360",
+    "url": "/react-posts-mas-votados/index.html"
+  }
+];
